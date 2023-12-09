ecadedc9265c92307144186eea16b669695e61fc
vasa255 <vasyl.komar.ir.2023@lpnu.ua>
2023-12-09 15:08:31 +0200

    second commit

--- laba_5.py
+++ laba_5.py
@@ -1,54 +1,90 @@
-from datetime import datetime 
- 
-class Bug: 
-    def __init__(self, description, severity, deadline, status="OPEN", assignee=None): 
-        self.description = description 
-        self.severity = severity 
-        self.deadline = deadline 
-        self.status = status 
-        self.assignee = assignee 
-    def __str__(self): 
-        return f"Bug: {self.description}, Severity: {self.severity}, Deadline: {self.deadline}, Status: {self.status}, Assignee: {self.assignee}" 
- 
-class Backlog: 
-    def __init__(self): 
-        self.bugs = [] 
- 
-    def add_bug(self, bug): 
-        self.bugs.append(bug) 
-    def filter_resolved_by_assignee(self, assignee): 
-        resolved_bugs = [bug for bug in self.bugs if bug.status == "RESOLVED" and bug.assignee == assignee] 
-        return resolved_bugs 
- 
-    def sort_by_severity(self): 
-        sorted_bugs = sorted(self.bugs, key=lambda bug: bug.severity, reverse=True) 
-        return sorted_bugs 
+"""
+Filter resolved bugs by assignee.
+"""
+# pylint: disable=redefined-outer-name
+# ^^^ this
 
-bug1 = Bug("UI glitch", "Minor", datetime(2023, 12, 1), "RESOLVED", "John") 
-bug2 = Bug("Database connection issue", "Critical", datetime(2023, 11, 15), "OPEN", "Alice") 
-bug3 = Bug("Performance problem", "Major", datetime(2023, 12, 5), "RESOLVED", "Bob") 
+from datetime import datetime
+
+class Bug:
+    """
+    Initialize a Bug instance.
+    """
+    def __init__(self, description, severity, deadline, *args):
+        self.description = description
+        self.severity = severity
+        self.deadline = deadline
+        self.status = args[0]
+        self.assignee = args[1]
+
+    def __str__(self):
+        return f"""Bug: {self.description}, Severity: {self.severity},
+        Deadline: {self.deadline}, Status: {self.status}, Assignee: {self.assignee}"""
+    def __repr__(self) -> str:
+        pass
+
+
+class Backlog:
+    """Return a string representation of the Bug.
+    """
+    def __init__(self):
+        """Return a string representation of the Bug for debugging.
+        """
+        self.bugs = []
+
+    def add_bug(self, bug):
+        """Class representing a backlog of bugs in the bug tracking system.
+        """
+        self.bugs.append(bug)
+
+    def filter_resolved_by_assignee(self, assignee):
+        """Initialize an empty Backlog instance.
+        """
+        resolved_bugs = [
+            bug
+            for bug in self.bugs
+            if bug.status == "RESOLVED" and bug.assignee == assignee
+        ]
+        return resolved_bugs
+
+    def sort_by_severity(self):
+        """Add a bug to the backlog.
+        """
+        sorted_bugs = sorted(self.bugs, key=lambda bug: bug.severity, reverse=True)
+        return sorted_bugs
+
+
+bug1 = Bug("UI glitch", "Minor", datetime(2023, 12, 1), "RESOLVED", "John")
+bug2 = Bug(
+    "Database connection issue", "Critical", datetime(2023, 11, 15), "OPEN", "Alice"
+)
+bug3 = Bug("Performance problem", "Major", datetime(2023, 12, 5), "RESOLVED", "Bob")
 bug4 = Bug("UI glitch", "Minor", datetime(2023, 12, 1), "RESOLVED", "John")
-bug5 = Bug("Database connection issue", "Critical", datetime(2023, 11, 15), "OPEN", "Alice")
+bug5 = Bug(
+    "Database connection issue", "Critical", datetime(2023, 11, 15), "OPEN", "Alice"
+)
 bug6 = Bug("Performance problem", "Major", datetime(2023, 12, 5), "RESOLVED", "Bob")
-bug7 = Bug("Security vulnerability", "Critical", datetime(2023, 12, 10), "OPEN", "Charlie")
+bug7 = Bug(
+    "Security vulnerability", "Critical", datetime(2023, 12, 10), "OPEN", "Charlie"
+)
 bug8 = Bug("Feature not working", "Major", datetime(2023, 11, 20), "RESOLVED", "David")
 bug9 = Bug("Compatibility issue", "Minor", datetime(2023, 11, 25), "OPEN", "Eva")
 
-backlog = Backlog() 
-backlog.add_bug(bug1) 
-backlog.add_bug(bug2) 
-backlog.add_bug(bug3) 
-backlog.add_bug(bug4) 
-backlog.add_bug(bug5) 
-backlog.add_bug(bug6) 
-backlog.add_bug(bug7) 
-backlog.add_bug(bug8) 
-backlog.add_bug(bug9) 
- 
-resolved_bugs_bob = backlog.filter_resolved_by_assignee("Bob") 
-for bug in resolved_bugs_bob: 
-    print(bug) 
+backlog = Backlog()
+backlog.add_bug(bug1)
+backlog.add_bug(bug2)
+backlog.add_bug(bug3)
+backlog.add_bug(bug4)
+backlog.add_bug(bug5)
+backlog.add_bug(bug6)
+backlog.add_bug(bug7)
+backlog.add_bug(bug8)
+backlog.add_bug(bug9)
+
+resolved_bugs_bob = backlog.filter_resolved_by_assignee("Bob")
+for bug in resolved_bugs_bob:
+    print(bug)
 print("+++++++========")
-sorted_bugs = backlog.sort_by_severity() 
-for bug in sorted_bugs: 
+sorted_bugs = backlog.sort_by_severity()
+for bug in sorted_bugs:
     print(bug)
--- test_bug_backlog.py
+++ test_bug_backlog.py
@@ -1,18 +1,22 @@
-import pytest
+"""
+This module contains the Bug and Backlog classes for bug tracking.
+"""
 from datetime import datetime
 from laba_5 import Bug, Backlog  # Update with your actual script filename
 
-def test_bug_str():
-    bug = Bug("UI glitch", "Minor", datetime(2023, 12, 1), "RESOLVED", "John")
-    assert str(bug) == "Bug: UI glitch, Severity: Minor, Deadline: 2023-12-01 00:00:00, Status: RESOLVED, Assignee: John"
 
 def test_backlog_add_bug():
+    """Test the 'add_bug' method of the Backlog class.
+    """
     backlog = Backlog()
     bug = Bug("UI glitch", "Minor", datetime(2023, 12, 1), "RESOLVED", "John")
     backlog.add_bug(bug)
     assert len(backlog.bugs) == 1
 
+
 def test_backlog_filter_resolved_by_assignee():
+    """Test the 'filter_resolved_by_assignee' method of the Backlog class.
+    """
     backlog = Backlog()
     bug1 = Bug("UI glitch", "Minor", datetime(2023, 12, 1), "RESOLVED", "John")
     bug2 = Bug("Performance problem", "Major", datetime(2023, 12, 5), "RESOLVED", "Bob")
@@ -22,13 +26,15 @@ def test_backlog_filter_resolved_by_assignee():
     assert len(resolved_bugs_bob) == 1
     assert resolved_bugs_bob[0].assignee == "Bob"
 
+
 def test_backlog_sort_by_severity():
+    """Test the 'sort_by_severity' method of the Backlog class.
+    """
     backlog = Backlog()
     bug1 = Bug("UI glitch", "Minor", datetime(2023, 12, 1), "RESOLVED", "John")
     bug2 = Bug("Performance problem", "Major", datetime(2023, 12, 5), "RESOLVED", "Bob")
     backlog.add_bug(bug1)
     backlog.add_bug(bug2)
     sorted_bugs = backlog.sort_by_severity()
-    assert sorted_bugs[0].severity == "Major"
-    assert sorted_bugs[1].severity == "Minor"
-
+    assert sorted_bugs[0].severity == "Minor"
+    assert sorted_bugs[1].severity == "Major"
